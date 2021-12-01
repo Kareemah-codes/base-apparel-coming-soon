17669a777c88c7ac6879849dd59b491a79fc4532
Kareemah-codes <krimmyninja@gmail.com>
2021-12-01 21:51:17 +0100

    done

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 
 
-const EMAIL_PATTERN = /[a-z A-Z 0-9 _ /-]*[@][a-z]*[.][a-z]{3,5}/;
-let userInput = document.getElementById("email-box").value;
+ const EMAIL_PATTERN = /[a-z A-Z 0-9 _ /-]*[@][a-z]*[.][a-z]{3,5}/;
+ let userInput = document.getElementById("email-box").value;
 
-function validateEmail(userInput){
-    let match = EMAIL_PATTERN.test(userInput);
-        if (!(match)){
-            document.getElementById("form").style.border = "2px solid hsl(0, 93%, 68%)";
-            document.getElementById("error-message").style.display = "block";
-            document.getElementById("error-icon").style.display = "block";
-        }
-}
-validateEmail(userInput);
+ function validateEmail(userInput){
+     let match = EMAIL_PATTERN.test(userInput);
+         if (!(match)){
+             document.getElementById("form").style.border = "2px solid hsl(0, 93%, 68%)";
+             document.getElementById("error-message").style.display = "block";
+             document.getElementById("error-icon").style.display = "block";
+         }
+ }
+ validateEmail(userInput);
+
